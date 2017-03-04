7984a0c05c5eeec517d2c8d9d84d0fbf6332f9e1
pavelZaletski <zaleckijpavel@gmail.com>
2017-03-04 18:14:12 +0300

    sdjbsdh

--- index.js
+++ index.js
@@ -10,29 +10,30 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+	response.render('pages/index');
 });
 
 app.get('/courses', function(request, response) {
-  response.json([{
-  	title: 'video course 1',
-  	duration: '1h 28min',
-  	date: 1488638630467,
-  	description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s',
-  	preview_img: 'https://epam-study.herokuapp.com/img/1.jpg'
-  }, {
-  	title: 'video course 2',
-  	duration: '50min',
-  	date: 1488648630459,
-  	description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s',
-  	preview_img: 'https://epam-study.herokuapp.com/img/2.jpg'
-  }, {
-  	title: 'video course 3',
-  	duration: '1h 28min',
-  	date: 1488678630163,
-  	description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s',
-  	preview_img: 'https://epam-study.herokuapp.com/img/3.jpg'
-  }])
+	res.header("Access-Control-Allow-Origin", "*");
+	response.json([{
+		title: 'video course 1',
+		duration: '1h 28min',
+		date: 1488638630467,
+		description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s',
+		preview_img: 'https://epam-study.herokuapp.com/img/1.jpg'
+	}, {
+		title: 'video course 2',
+		duration: '50min',
+		date: 1488648630459,
+		description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s',
+		preview_img: 'https://epam-study.herokuapp.com/img/2.jpg'
+	}, {
+		title: 'video course 3',
+		duration: '1h 28min',
+		date: 1488678630163,
+		description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s',
+		preview_img: 'https://epam-study.herokuapp.com/img/3.jpg'
+	}]);
 });
 
 app.listen(app.get('port'), function() {
